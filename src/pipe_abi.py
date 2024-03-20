1205818f86dcb9710799221440f481cd155441ec
Aki Tuomi <cmouse@cmouse.fi>
2024-03-21 00:40:23 +0200

    Fix test abi to match new API

--- src/pipe_abi.py
+++ src/pipe_abi.py
@@ -5,12 +5,12 @@ import pdns.remotebackend.unix
 
 
 class MyHandler(pdns.remotebackend.Handler):
-    def do_lookup(self, args):
-        if (args['qname'] == 'test.com' and args['qtype'] in ('ANY', 'A')):
+    def do_lookup(self, qname='', qtype='', **kwargs):
+        if (qname == 'test.com' and qtype in ('ANY', 'A')):
             self.result = []
             self.result.append(self.record('test.com', 'A', '127.0.0.1',
                                            ttl=300))
-        if (args['qname'] == 'test.com' and (args['qtype'] in ('ANY', 'SOA'))):
+        if (qname == 'test.com' and (qtype in ('ANY', 'SOA'))):
             self.result = []
             self.result.append(
                 self.record('test.com', 'SOA',
